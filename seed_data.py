2252fa4ae27f4f0912148af0956eb5aa015ad762
Pablo V <pvalfos@gmail.com>
2023-03-09 19:21:07 -0300

    Se habilita el admin y se agrega herencia de templates - Clase 20

---
+++ seed_data.py
@@ -0,0 +1,39 @@
+from SocialTravel.models import Post
+
+Post(carousel_caption_title = "Un Carousel Title",
+     carousel_caption_description = "Un Carousel Description",
+     heading = "Mi viaje",
+     description = "Una Descripcion",
+     un_campo = ""
+     ).save()
+
+Post(carousel_caption_title = "Un Carousel Title1",
+     carousel_caption_description = "Un Carousel Description1",
+     heading = "Mi viaje1",
+     description = "Una Descripcion1",
+     un_campo = ""
+     ).save()
+
+Post(carousel_caption_title = "Un Carousel Title2",
+     carousel_caption_description = "Un Carousel Description2",
+     heading = "Mi viaje2",
+     description = "Una Descripcion2",
+     un_campo = ""
+     ).save()
+
+Post(carousel_caption_title = "Un Carousel Title3",
+     carousel_caption_description = "Un Carousel Description3",
+     heading = "Mi viaje3",
+     description = "Una Descripcion3",
+     un_campo = ""
+     ).save()
+
+
+
+#for _ in range(0, 100):
+#    Post(carousel_caption_title = "Un Carousel Title",
+#        carousel_caption_description = "Un Carousel Description",
+#        heading = "Mi viaje",
+#        description = "Una Descripcion",
+#        un_campo = ""
+#        ).save()
